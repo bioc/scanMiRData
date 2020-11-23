6e5a42c052e93b2449b5dfc25753e2c5895c8405
plger <pl.germain@gmail.com>
2020-11-23 15:43:17 +0000

    added metadata and filtering

--- R/getKdModels.R
+++ R/getKdModels.R
@@ -1,8 +1,15 @@
 #' getKdModels
 #'
-#' Returns a collection of miRNA KdModels for the requested species.
+#' Returns a collection of miRNA KdModels for the requested species. 
+#' \underline{Note that by default, low-confidence miRNAs are not returned} 
+#' (see `categories`).
 #'
 #' @param species Either "hsa" (human), "mmu" (mouse) or "rno" (rat)
+#' @param categories The (mirbase) categories of miRNAs to return. Any 
+#' combination of "Low-confidence", "Poorly conserved", 
+#' "Conserved across mammals", "Conserved across vertebrates". If NULL, all
+#' categories are returned. miRNAs with NA as conservation are not returned
+#' unless `categories==NULL`.
 #'
 #' @return An object of class \code{\link[scanMiR]{KdModelList}}
 #' @export
@@ -10,7 +17,22 @@
 #' @examples
 #' mods <- getKdModels("rno")
 #' summary(mods)
-getKdModels <- function(species=c("hsa","mmu","rno")){
+getKdModels <- function( species=c("hsa","mmu","rno"), 
+                         categories=c( "Poorly conserved", 
+                                       "Conserved across mammals", 
+                                       "Conserved across vertebrates") ){
+  cats <- c("Low-confidence","Poorly conserved","Conserved across mammals",
+            "Conserved across vertebrates")
+  if(!is.null(categories))
+    categories <- match.arg(categories, cats, several.ok=TRUE)
   species <- match.arg(species)
-  get(data(species))
+  mods <- get(data(species))
+  if(is.null(categories)) return(mods)
+  con <- conservation(mods)
+  if(all(is.na(con))){
+    warning("The collection has no conservation information; the `categories`",
+            " argument will be ignored.")
+    return(mods)
+  }
+  mods[con %in% categories]
 }
